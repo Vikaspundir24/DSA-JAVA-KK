9050753b87518ed8a712da34e6d56ea7d387d610
vikas pundir <vikaspundiruser@gmail.com>
2025-06-24 12:48:47 +0530

    floor ceiling

--- binarySearch/src/floorOfaNumber.java
+++ binarySearch/src/floorOfaNumber.java
@@ -1,16 +1,12 @@
-public class ceilingOfNumber {
+public class floorOfaNumber {
     public static void main(String[] args) {
         //cieling  =  smalled element in the array greater or = target
         int [] arr = {2,3,5,9,14,16,18};
         int target = 15;
-        int ans = ceil(arr, target);
+        int ans = floor(arr, target);
         System.out.println(ans);
     }
-    static int ceil(int[] arr, int target){
-        //but what if the target is greator than the greatest number in the aray
-        if(target > arr[arr.length-1]){
-            return -1;
-        }
+    static int floor(int[] arr, int target){
         int start =0;
         int end = arr.length-1;
 
@@ -22,11 +18,13 @@ public class ceilingOfNumber {
             } else if (target< arr[mid]) {
                 end = mid - 1;
             }
-            else {
-                return mid;
+            else{
+                return mid; //ans found
             }
         }
 
-        return start;
+        return end;
     }
+
 }
+
