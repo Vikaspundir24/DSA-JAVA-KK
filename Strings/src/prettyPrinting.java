941ad5096cc91ae2ca7afee0d46b58351a221005
Vikas Pundir <vikaspundiruser@gmail.com>
2025-08-24 21:56:50 +0530

    strings

---
+++ Strings/src/prettyPrinting.java
@@ -0,0 +1,10 @@
+public class prettyPrinting {
+    public static void main(String[] args) {
+        float a = 3.1288f;
+        //System.out.printf("formatted number is %.2f", a);
+
+         //System.out.printf("pi : %.3f", Math.PI);
+
+        System.out.printf("hello my name is %s and I am a %s  ", "vikas", "photographer");
+    }
+}
