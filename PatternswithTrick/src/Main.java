c71c8deb53c41424b7ab07d3475a29992f889874
Vikas Pundir <vikaspundiruser@gmail.com>
2025-08-27 17:09:46 +0530

    patterns

--- PatternswithTrick/src/Main.java
+++ PatternswithTrick/src/Main.java
@@ -1,8 +1,10 @@
 public class Main{
     public static void main(String[] args) {
-        pattern1(3);
+        //pattern2(5);
+        //pattern1(5);
+        pattern3(5);
     }
-    static void pattern1(int n){
+    static void pattern2(int n){
         for (int row = 0 ; row <= n; row++){
             //for every row, rum the col
             for (int col = 0; col <=row; col++){
@@ -12,4 +14,25 @@ public class Main{
             System.out.println("");
         }
     }
+    static void pattern1(int n){
+        for (int row = 0 ; row <= n; row++){
+            //for every row, rum the col
+            for (int col = 0; col <=n; col++){
+                System.out.print("* ");
+            }
+            // when one row is printer , we need to add a new line
+            System.out.println("");
+        }
+    }
+    static void pattern3(int n){
+        for (int row = 0 ; row <= n; row++){
+            //for every row, rum the col
+            for (int col = n; col >row; col--){
+                System.out.print("* ");
+            }
+            // when one row is printer , we need to add a new line
+            System.out.println("");
+        }
+    }
+
 }
