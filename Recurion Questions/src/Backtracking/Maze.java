9d317f5dd6fdc40456b99e2502b30f4c550f9edd
Vikas Pundir <vikaspundiruser@gmail.com>
2025-09-08 22:59:56 +0530

    PATTERNS OF MAZE PROBLEMS

--- Recurion Questions/src/Backtracking/Maze.java
+++ Recurion Questions/src/Backtracking/Maze.java
@@ -9,7 +9,9 @@ public class Maze {
         //System.out.println(count(3,3));
         // path("",3,3);
         //System.out.println(pathRet("",3,3));
-        System.out.println(pathRetDiagonal("",3,3));
+        //System.out.println(pathRetDiagonal("",3,3));
+        boolean[][] board = {{true,true,true},{true, false,true},{true,true,true}};
+        pathRestrictions("",board,0,0);
     }
 
 
@@ -71,6 +73,23 @@ public class Maze {
             list.addAll(pathRetDiagonal(p+'H', r,c-1));
         }
         return list;
+
+    }
+
+    static void pathRestrictions(String p, boolean [][] maze, int r, int c){
+        if(r == maze.length-1 && c==maze[0].length-1 ){
+            System.out.println(p);
+            return;
+        }
+        if (maze[r][c] == false ){
+            return;
+        }
+        if (r < maze.length-1){
+            pathRestrictions(p+'D',maze, r+1,c);
+        }
+        if(c< maze[0].length-1){
+            pathRestrictions(p+"R",maze,r,c+1);
+        }
 
     }
 }
