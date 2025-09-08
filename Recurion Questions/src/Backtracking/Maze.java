9f5e2a1cd96a0c79d7f3f9ccc5ffae9607e015fd
Vikas Pundir <vikaspundiruser@gmail.com>
2025-09-08 22:35:16 +0530

    lets goooo

--- Recurion Questions/src/Backtracking/Maze.java
+++ Recurion Questions/src/Backtracking/Maze.java
@@ -8,7 +8,8 @@ public class Maze {
     public static void main(String[] args) {
         //System.out.println(count(3,3));
         // path("",3,3);
-        System.out.println(pathRet("",3,3));
+        //System.out.println(pathRet("",3,3));
+        System.out.println(pathRetDiagonal("",3,3));
     }
 
 
@@ -48,6 +49,26 @@ public class Maze {
         }
         if(c>1){
             list.addAll(pathRet(p+'R', r,c-1));
+        }
+        return list;
+
+    }
+
+    static ArrayList<String> pathRetDiagonal(String p, int r, int c){
+        if(r == 1 && c==1 ){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1 && c>1){
+            list.addAll(pathRetDiagonal(p+'D', r-1,c-1));
+        }
+        if (r > 1){
+            list.addAll(pathRetDiagonal(p+'V', r-1,c));
+        }
+        if(c>1){
+            list.addAll(pathRetDiagonal(p+'H', r,c-1));
         }
         return list;
 
