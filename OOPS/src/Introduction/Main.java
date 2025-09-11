7441671412a649c5663f949f51d1934ed37a05bf
Vikas Pundir <vikaspundiruser@gmail.com>
2025-09-11 23:17:25 +0530

    oops

---
+++ OOPS/src/Introduction/Main.java
@@ -0,0 +1,15 @@
+package Introduction;
+
+public class Main{
+    public static void main(String[] args) {
+
+    }
+
+
+}
+//Creating a class
+class  Student {
+    int [] rno = new int[5];
+    String [] name = new String[5];
+    float []  marks = new float[5];
+}
