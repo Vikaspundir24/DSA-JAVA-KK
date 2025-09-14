f66478d9015e103311415ae607eb63bc14861438
Vikas Pundir <vikaspundiruser@gmail.com>
2025-09-14 14:45:09 +0530

    oop

--- OOPS/src/Introduction/Main.java
+++ OOPS/src/Introduction/Main.java
@@ -7,18 +7,22 @@ public class Main{
         //just declaring
         //Student Vikas;
         //Vikas = new Student();
-        Student vikas  = new Student(12,"VIKAS PUNDIR", 99.4f);
-        Student ramnath =  new Student();
+        Student vikas  = new Student();
+        //Student ramnath =  new Student(10,"Ramnath Kumar", 99.f);
 //        vikas.rno = 12;
 //        vikas.name = "Vikas pundir";
 //        vikas.marks= 99.5f;
-//        vikas.changeName("VP");
-//        vikas.greeting();
+    //    ramnath.changeName("Suraj");
+     //   vikas.greeting();
+       // ramnath.greeting();
 
-        System.out.println(vikas.rno);
-        System.out.println(vikas.name);
-        System.out.println(vikas.marks);
-
+//        System.out.println(vikas.rno);
+//        System.out.println(vikas.name);
+//        System.out.println(vikas.marks);
+        Student random = new Student(vikas);
+       // System.out.println(random.name);
+        Student random2   = new Student();
+        System.out.println(random2.name);
     }
 
 }
@@ -36,14 +40,28 @@ class Student {
     void changeName(String newName){
         this.name = newName;
     }
+//    Student(){
+//        this.rno = 12;
+//        this.name = "Vikas pundir";
+//        this.marks= 99.5f;
+//    }
+
     Student(){
-        this.rno = 12;
-        this.name = "Vikas pundir";
-        this.marks= 99.5f;
+       //thisis how you call a constructor from another constructor
+        // internally : new Student()
+        this(13,"Default person", 100.0f);
     }
+
+    //Student arpit =new Student (17,"Arpit", 89.f);
+    // here, this will be replaced with arpit
     Student(int rno, String name, float marks ){
         this.rno = rno;
         this.name = name;
         this.marks= marks;
+    }
+    Student(Student other){
+        this.name = other.name;
+        this.rno = other.rno;
+        this.marks = other.marks;
     }
 }
